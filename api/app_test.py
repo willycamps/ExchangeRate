165a56c52f36fa4aeb34d43dbac5ea38b809dbc1
willycamps <willycamps@gmail.com>
2023-05-30 07:14:29 +0200

    update api test

--- api/app_test.py
+++ api/app_test.py
@@ -5,7 +5,6 @@ from app import app
 @pytest.fixture
 def client():
     app.config.update({'TESTING': True})
-
     with app.test_client() as client:
         yield client
 
@@ -15,4 +14,10 @@ def test_getcodes(client):
     assert resp.status_code == 201
    
     
-
+def test_getexchange(client):
+    response = client.post("/getExchange", data={
+        "amount": "100",
+        "from": "EUR",
+        "to": "USD",
+    })
+    assert response.status_code == 201
